6722e9eb30f47b0899b083c772ebf845aae42449
963476771@qq.com <963476771@qq.com>
2022-06-25 19:41:34 +0800

    v1.0.7

--- src/js/details.js
+++ src/js/details.js
@@ -17,6 +17,7 @@
  </span>`
      $('.mainprice').html(text);
      $('.imgZoom>a>img').attr('src', res.picture);
+     $('.big>img').attr('src', res.picture);
      let template = '';
      pic.forEach(el => {
          //  console.log(el.src);
@@ -89,3 +90,94 @@
      // 将数据添加到cookie中
      cookie.set('shop', JSON.stringify(shop));
  }
+
+
+ // 放大镜
+ //  $(function() {
+ //      (function() {
+ //          var ulobj = $(".thumbnail-box ul");
+ //          var picimg = $(".imgZoom a img");
+ //          var objimg = $(".imgpart .bigpic img");
+ //          ulobj.on("mouseenter", "li", function() {
+ //              var imgsrc = $(this).children("img").attr("src");
+ //              $(this).addClass("active").siblings().removeClass("active");
+ //              picimg.attr("src", imgsrc);
+ //              objimg.attr("src", imgsrc)
+ //          });
+ //          var pic = $(".imgpart .pic");
+ //          var magnify = $(".imgpart .pic .magnify");
+ //          var bigpic = $(".imgpart .bigpic");
+ //          var objimg = $(".imgpart .bigpic img");
+ //          pic.mousemove(function(e) {
+ //              magnify.show();
+ //              bigpic.show();
+ //              var pagex = e.pageX;
+ //              var pagey = e.pageY;
+ //              var pictop = pic.offset().top;
+ //              var picleft = pic.offset().left;
+ //              var magnifyw = magnify.width();
+ //              var magnifyh = magnify.height();
+ //              var magnifytop = pagey - pictop - magnifyh / 2;
+ //              var magnifyleft = pagex - picleft - magnifyw / 2;
+ //              var picw = pic.width() - magnifyw;
+ //              var pich = pic.height() - magnifyh;
+ //              magnifytop = magnifytop < 0 ? 0 : magnifytop;
+ //              magnifyleft = magnifyleft < 0 ? 0 : magnifyleft;
+ //              magnifytop = magnifytop > pich ? pich : magnifytop;
+ //              magnifyleft = magnifyleft > picw ? picw : magnifyleft;
+ //              magnify.css({ top: magnifytop, left: magnifyleft });
+ //              var minl = bigpic.width() - objimg.width();
+ //              var mint = bigpic.height() - objimg.height();
+ //              var objimgl = -magnifyleft * 2;
+ //              var objimgt = -magnifytop * 2;
+ //              objimgl = objimgl < minl ? minl : objimgl;
+ //              objimgt = objimgt < mint ? mint : objimgt;
+ //              objimg.css({ top: objimgt, left: objimgl })
+ //          });
+ //          pic.mouseleave(function() {
+ //              magnify.hide();
+ //              bigpic.hide()
+ //          })
+ //      })()
+ //  });
+
+
+ //放大镜
+ $('.imgZoom>a').on('mouseover', function() {
+         $('.big>img').css('display', 'block');
+         $('.glass').css('display', 'block');
+         $(this).on('mousemove', function(ev) {
+             //  ev.stopPropagation();
+             let offsetX = ev.offsetX;
+             let offsetY = ev.offsetY;
+             let height = $('.glass').outerHeight();
+             let width = $('.glass').outerWidth();
+             console.log($('.glass').innerWidth());
+             let left = offsetX - width / 2;
+             //  console.log(left);
+             let top = offsetY - height / 2;
+             //  console.log(top);
+             let maxWidth = $('.small-img').outerWidth() - $('.glass').outerWidth();
+             let maxHeight = $('.small-img').outerHeight() - $('.glass').outerHeight();
+             //边界
+             left = left < 0 ? 0 : left;
+             top = top < 0 ? 0 : top;
+
+             left = left > maxHeight ? maxHeight : left;
+             top = top > maxWidth ? maxHeight : top;
+
+             $('.glass').css('left', left + 'px');
+             $('.glass').css('top', top + 'px');
+
+             let bleft = -2 * (left - 85);
+             let btop = -2 * (top - 85);
+
+             $('.big>img').css('left', bleft + 'px');
+             $('.big>img').css('top', btop + 'px');
+
+         })
+     })
+     //  $('.imgZoom>a').on('mouseleave', function() {
+     //      $('.glass').css('display', 'none');
+     //      $('.big>img').css('display', 'none');
+     //  })
--- src/js/shopCart.js
+++ src/js/shopCart.js
@@ -1,11 +1,11 @@
 import $ from './lib/jquery.esm.js';
 import cookie from './lib/cookie.js';
-
+//从cookie中拿到在详情页中存储的商品 id 和 商品 num
 let shop = cookie.get('shop');
 shop = JSON.parse(shop);
-// console.log(shop);
+//获得所有的id 
 let idList = shop.map(el => el.id).join();
-
+// 通过ajax请求 请求到商品数据 进行页面渲染
 $.ajax({
         type: "get",
         url: "../interface/getShop.php",
@@ -69,49 +69,141 @@ $.ajax({
         `
         });
         $('.item').html(template);
+        // 实现删除 
+        // 通过判断当前商品id在shop中的id 筛选中 不是当前id的商品
         $('.remove').on('click', function() {
-                let res = shop.filter(el => el.id != $(this).attr('data-id'));
-                console.log(res);
-                cookie.set('shop', JSON.stringify(res));
-                location.reload();
-            })
-            //  点击按钮增加或减少
-        $('.item-main').on('click', '.plus', function(ev) {
-            $($(ev.target).prevAll('.minus')).css('cursor', 'pointer');
-            let count = $($(ev.target).prev()).val();
-            $($(ev.target).prev()).val(++count);
-            //小计价格
-            $(ev.target).parents('.td-price').next().find('em').text($($(ev.target).prev()).val() * parseInt($(ev.target).parents('.td-amount').prev().find('em').text()))
-        });
-        $('.item-main').on('click', '.minus', function(ev) {
-                let count = $($(ev.target).next()).val();
-                if (count > 1) {
-                    $($(ev.target).next()).val(--count);
-                    $(ev.target).parents('.td-price').next().find('em').text($($(ev.target).next()).val() * parseInt($(ev.target).parents('.td-amount').prev().find('em').text()));
-                } else {
-                    $(this).css('cursor', 'no-drop');
-                    $(this).off();
-                }
-            })
-            //计算结算的总价
-        let sum = 0;
-        $('.cart-checkbox>input').on('click', function() {
+            let res = shop.filter(el => el.id != $(this).attr('data-id'));
+
+            // 将筛选后的商品结果 重新设置到cookie中
+            cookie.set('shop', JSON.stringify(res));
+
+            // 刷新 实现  筛选商品的渲染  从而删除当前需要删除的商品
+            location.reload();
+        })
+
+        //计算结算的总价
+
+
+        $('.check').on('click', function() {
+            // 获得当前结算位置的价格
+            let sum = parseInt($('.total').text());
+            // 判断是否有select class属性
             if ($(this).parents('.item-main').hasClass('select')) {
                 $(this).parents('.item-main').removeClass('select')
                 sum -= parseInt($(this).parents('.item-main').find('.sum').text());
+                console.log(parseInt($(this).parents('.item-main').find('.sum').text()));
             } else {
                 $(this).parents('.item-main').addClass('select');
                 sum += parseInt($(this).parents('.item-main').find('.sum').text());
+                console.log(parseInt($(this).parents('.item-main').find('.sum').text()));
             }
+
             // 结算价格
             $($('.zprice').next()).text(sum)
         })
+
+        //  点击按钮增加或减少
+        $('.item-main').on('click', '.plus', function(ev) {
+
+            //获得当前选中的需要结算的商品的价格
+            let sum1 = parseInt($('.total').text());
+
+            // 判断当前这个商品是否被选中
+            if ($(ev.target).parents('.item-main').hasClass('select')) {
+
+                // 叠加当前需要结算商品的总价格
+                sum1 += parseInt($(ev.target).parents('.item-main').find('.price-now>em').text());
+
+                // 设置当前结算的价格
+                $($('.zprice').next()).text(sum1)
+            }
+
+            // 设置当前 减少 按钮从禁止点击 到可以点击
+            $($(ev.target).prevAll('.minus')).css('cursor', 'pointer');
+
+            // 获得当前商品已添加的数量
+            let count = $($(ev.target).prev()).val();
+
+            // 点击增加按钮后 增加数量 改变当前数量的显示
+            $($(ev.target).prev()).val(++count);
+
+            //小计价格
+            $(ev.target).parents('.td-price').next().find('em').text($($(ev.target).prev()).val() * parseInt($(ev.target).parents('.td-amount').prev().find('em').text()))
+        });
+
+        // 点击减少 按钮
+        $('.item-main').on('click', '.minus', function(ev) {
+
+            //获得当前选中的需要结算的商品的价格
+            let sum1 = parseInt($('.total').text());
+            if ($(ev.target).parents('.item-main').hasClass('select')) {
+                // 判断当前需要结算的价格是否小于等于 0  小于等于0时 则不能继续减少当前总结算价格
+                if (sum1 <= 0) {
+                    sum1 = 0;
+                }
+                sum1 -= parseInt($(ev.target).parents('.item-main').find('.price-now>em').text());
+                $($('.zprice').next()).text(sum1)
+            }
+            //获得当前商品在购物车中的数量
+            let count = $($(ev.target).next()).val();
+
+            // 当商品数量大于1时 进行 右边小计的计算
+            if (count > 1) {
+                $($(ev.target).next()).val(--count);
+                $(ev.target).parents('.td-price').next().find('em').text($($(ev.target).next()).val() * parseInt($(ev.target).parents('.td-amount').prev().find('em').text()));
+            } else {
+
+                // 设置当前 减少按钮 禁用图标显示 不可以在被点击
+                $(this).css('cursor', 'no-drop');
+                $(this).off();
+            }
+        })
+
+        // 全选
+        $('.AllCheckBox').on('click', function() {
+
+            //全选后计算 结算价格  先trigger后有效果 不先执行的话 复选框没有勾选显示
+            $('.check').trigger('click');
+
+            //  通过全选按钮的 选中状态 为所有选项框 添加 选中对应的选中状态 实现全选和不全选
+            $('.item').children().find('.check').prop('checked', $(this).prop('checked'));
+            $('.AllCheckBox').prop('checked', $(this).prop('checked'));
+
+            //已选商品的数量显示
+            //判断是否全选
+            if ($('.AllCheckBox').prop('checked')) {
+
+                // 全选则 显示已勾选的商品总数
+                $('.select-goods>b').text($('.check').length);
+            } else {
+
+                //不全选 则为0
+                $('.total').text('0.00');
+                $('.select-goods>b').text('0');
+            }
+        });
+
+        // 一个未选中 取消全选的勾选状态
+        $('.item').on('click', '.check', function() {
+            // 点击勾选 进行 已选商品的数量显示
+            $('.select-goods>b').text($('.select').length);
+            $('.AllCheckBox').prop('checked', allCheck());
+        })
+
+        // 判断当前所有的复选框是否都被选中 是则返回true 
+        function allCheck() {
+            let items = $('.item').children().find('.check');
+            let res = Array.from(items);
+            return res.every(el => $(el).prop('checked'));
+        }
+
     }).catch(xhr => {
         console.log(xhr.status);
     })
 
 
-// 导航栏二级菜单
+// 导航栏二级菜单下拉
+
 $('.hover-box').on('mouseenter', function() {
     $(this).children('div').delay(100).show(100);
     $(this).children('a').addClass('header-active-hover');
@@ -120,19 +212,3 @@ $('.hover-box').on('mouseleave', function() {
     $(this).children('div').delay(100).hide(100);
     $(this).children('a').removeClass('header-active-hover');
 })
-
-// 全选
-$('.AllCheckBox').on('click', function() {
-    $('.item').children().find('.check').prop('checked', $(this).prop('checked'));
-    $('.AllCheckBox').prop('checked', $(this).prop('checked'));
-});
-// 一个未选中 取消全选的勾选状态
-$('.item').on('click', '.check', function() {
-    $('.AllCheckBox').prop('checked', allCheck());
-})
-
-function allCheck() {
-    let items = $('.item').children().find('.check');
-    let res = Array.from(items);
-    return res.every(el => $(el).prop('checked'));
-}
